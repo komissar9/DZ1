12de48351bfd6a0a6a84c694667f7d565395ab66
Alena <magii.alena@yandex.ru>
2023-06-28 08:27:53 +0300

    Решение 8 задачи

--- DZ.cs
+++ DZ.cs
@@ -1,4 +1,4 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+﻿// 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
 
 // Console.Write("Введите первое число: ");
 // double firstDigit = Convert.ToDouble(Console.ReadLine());
@@ -13,7 +13,7 @@
 // {
 //     Console.WriteLine("Число " + secondDigit + " больше числа " + firstDigit);
 // }
-//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+//4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
 
 // Console.Write("Введите первое число: ");
 // double firstDigit = Convert.ToDouble(Console.ReadLine());
@@ -26,16 +26,25 @@
 // if (max < thirdDigit) max = thirdDigit;
 // Console.Write("Максимальное число: " + max);
 
-//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
-Console.Write("Введите число: ");
-double numberUser = Convert.ToDouble(Console.ReadLine());
-if (numberUser % 2 == 0)
- {
-    Console.WriteLine("Число " + numberUser + " четное");
- }
-else 
-{
-    Console.WriteLine("Число " + numberUser + " нечетное");
-}
-//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+//6. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+// Console.Write("Введите число: ");
+// double numberUser = Convert.ToDouble(Console.ReadLine());
+// if (numberUser % 2 == 0)
+//  {
+//     Console.WriteLine("Число " + numberUser + " четное");
+//  }
+// else 
+// {
+//     Console.WriteLine("Число " + numberUser + " нечетное");
+// }
 
+//8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.Write("Введите число: ");
+int numberUser = Convert.ToInt32(Console.ReadLine());
+int evenDigit = 2;
+Console.WriteLine("Все четные числа до " + numberUser + ":");
+while (evenDigit < numberUser)
+{
+    Console.WriteLine(evenDigit);
+    evenDigit += 2;
+}
