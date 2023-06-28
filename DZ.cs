4bb1a64aab7431c08644347da6a75180816b3321
Alena <magii.alena@yandex.ru>
2023-06-28 07:08:55 +0300

    Решение 4 задачи с дробными числами

---
+++ DZ.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+// Console.Write("Введите первое число: ");
+// double firstDigit = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// double secondDigit = Convert.ToDouble(Console.ReadLine());
+// if (firstDigit > secondDigit)
+// {
+//     Console.WriteLine("Число " + firstDigit + " больше числа " + secondDigit);
+// }
+// else
+// {
+//     Console.WriteLine("Число " + secondDigit + " больше числа " + firstDigit);
+// }
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("Введите первое число: ");
+double firstDigit = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double secondDigit = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double thirdDigit = Convert.ToDouble(Console.ReadLine());
+double max = firstDigit;
+if (max < secondDigit) max = secondDigit;
+if (max < thirdDigit) max = thirdDigit;
+Console.Write("Максимальное число: " + max);
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
