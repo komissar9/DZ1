6e70d4fa180bb6f3b91e3540bc9564b27e9f1999
Alena <magii.alena@yandex.ru>
2023-06-28 08:29:03 +0300

    end commit dz1

--- DZ.cs
+++ DZ.cs
@@ -1,42 +1,40 @@
 ﻿// 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.Write("Введите первое число: ");
+double firstDigit = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double secondDigit = Convert.ToDouble(Console.ReadLine());
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine("Число " + firstDigit + " больше числа " + secondDigit);
+}
+else
+{
+    Console.WriteLine("Число " + secondDigit + " больше числа " + firstDigit);
+}
 
-// Console.Write("Введите первое число: ");
-// double firstDigit = Convert.ToDouble(Console.ReadLine());
-
-// Console.Write("Введите второе число: ");
-// double secondDigit = Convert.ToDouble(Console.ReadLine());
-// if (firstDigit > secondDigit)
-// {
-//     Console.WriteLine("Число " + firstDigit + " больше числа " + secondDigit);
-// }
-// else
-// {
-//     Console.WriteLine("Число " + secondDigit + " больше числа " + firstDigit);
-// }
-//4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
-
-// Console.Write("Введите первое число: ");
-// double firstDigit = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Введите второе число: ");
-// double secondDigit = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Введите третье число: ");
-// double thirdDigit = Convert.ToDouble(Console.ReadLine());
-// double max = firstDigit;
-// if (max < secondDigit) max = secondDigit;
-// if (max < thirdDigit) max = thirdDigit;
-// Console.Write("Максимальное число: " + max);
+//4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисе
+Console.Write("Введите первое число: ");
+double firstDigit = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double secondDigit = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double thirdDigit = Convert.ToDouble(Console.ReadLine());
+double max = firstDigit;
+if (max < secondDigit) max = secondDigit;
+if (max < thirdDigit) max = thirdDigit;
+Console.Write("Максимальное число: " + max);
 
 //6. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
-// Console.Write("Введите число: ");
-// double numberUser = Convert.ToDouble(Console.ReadLine());
-// if (numberUser % 2 == 0)
-//  {
-//     Console.WriteLine("Число " + numberUser + " четное");
-//  }
-// else 
-// {
-//     Console.WriteLine("Число " + numberUser + " нечетное");
-// }
+Console.Write("Введите число: ");
+double numberUser = Convert.ToDouble(Console.ReadLine());
+if (numberUser % 2 == 0)
+ {
+    Console.WriteLine("Число " + numberUser + " четное");
+ }
+else 
+{
+    Console.WriteLine("Число " + numberUser + " нечетное");
+}
 
 //8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
 Console.Write("Введите число: ");
